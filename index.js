2dcc598e2b933d6d1669e892afbb9e74f44daf07
Gene Gittings <ggitting@iu.edu>
2019-06-19 17:53:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = { name, height
 }
 
 if (name === "Susan") {
-  condole.log("The name is correct")
+  console.log("The name is correct")
 } else{
   console.log("Expected the name to be Susan")
 }
